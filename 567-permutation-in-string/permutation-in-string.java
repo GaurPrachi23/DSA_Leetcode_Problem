748dfeed8ef32e2c30fe2eced55766fcb6d034d8
GaurPrachi23 <gaurprachi835@gmail.com>
2026-08-04 00:42:56 +0530

    Time: 7 ms (69.43%) | Memory: 43.7 MB (93.34%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+      if (s1.length() > s2.length()) {
+        return false;
+    }
+      int[] s1Freq = new int[26];
+      int[] windowFreq = new int[26];
+      int windowSize = s1.length(); 
+      for(int i = 0; i < windowSize; i++){
+        s1Freq[s1.charAt(i) - 'a']++;
+        windowFreq[s2.charAt(i) - 'a']++;
+      }
+      if (Arrays.equals(s1Freq, windowFreq)) {
+        return true;
+    } 
+      
+      for (int i = windowSize; i < s2.length(); i++){
+        windowFreq[s2.charAt(i - windowSize) - 'a']--;
+        windowFreq[s2.charAt(i) - 'a']++;
+        if (Arrays.equals(s1Freq, windowFreq)) {
+            return true;
+        }
+      }
+      return false;
+    }
+}
