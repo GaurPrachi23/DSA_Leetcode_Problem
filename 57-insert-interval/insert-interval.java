9320be60f19aca5709c7b8b1a557005c8bdfac8c
GaurPrachi23 <gaurprachi835@gmail.com>
2026-08-15 00:29:36 +0530

    Time: 1 ms (98.15%) | Memory: 47 MB (76.02%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,32 +1,43 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
 
+        int newStart = newInterval[0];
+        int newEnd = newInterval[1];
+
         ArrayList<int[]> ans = new ArrayList<>();
 
-        int i = 0;
-        int n = intervals.length;
+        for (int i = 0; i < intervals.length; i++) {
 
-        
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            ans.add(intervals[i]);
-            i++;
+            int currentStart = intervals[i][0];
+            int currentEnd = intervals[i][1];
+
+            
+            if (currentEnd < newStart) {
+                ans.add(new int[]{currentStart, currentEnd});
+            }
+
+            
+            else if (currentStart > newEnd) {
+
+                ans.add(new int[]{newStart, newEnd});
+
+                
+                for (int j = i; j < intervals.length; j++) {
+                    ans.add(intervals[j]);
+                }
+
+                return ans.toArray(new int[ans.size()][]);
+            }
+
+            
+            else {
+                newStart = Math.min(newStart, currentStart);
+                newEnd = Math.max(newEnd, currentEnd);
+            }
         }
 
         
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-
-        
-        ans.add(newInterval);
-
-        
-        while (i < n) {
-            ans.add(intervals[i]);
-            i++;
-        }
+        ans.add(new int[]{newStart, newEnd});
 
         return ans.toArray(new int[ans.size()][]);
     }
