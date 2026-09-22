41a5ea549abb38bdc60c4498b20fa2d1ab2b8afe
GaurPrachi23 <gaurprachi835@gmail.com>
2026-09-22 23:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (12.89%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        generate(current, result, k, n, 1);
+        return result;
+    }
+    public void generate(List<Integer> current, List<List<Integer>> result, int k, int remaining, int start){
+        if(remaining < 0){
+            return;
+        }
+        if(current.size() == k ){
+            if(remaining == 0){
+            result.add(new ArrayList<>(current));
+            }
+            return;
+        }
+        for(int i = start; i <= 9; i++){
+            current.add(i);
+            remaining -= i;
+            generate(current, result, k, remaining, i + 1);
+            remaining += i;
+            current.remove(current.size() - 1);
+        }
+    }
+}
